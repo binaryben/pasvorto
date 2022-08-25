14022cac17f4f44104a1dde76590b99859442d13
Benny Michaels <12827556+binaryben@users.noreply.github.com>
2022-08-25 13:34:46 +0800

    ✨ Saving from an old project - might be relevant?

---
+++ src/characters.ts
@@ -0,0 +1,11 @@
+// May be used in the future for generating passwords
+// where passphrases are unusable for whatever reason
+
+export const characters = {
+  lowerChars:   ['a','b','c','d','e','f','g','h','j','k','m', 'n','p','q','r','s','t','w','x','y','z'],
+  ambLower:     ['i','l','o','u','v'],
+  upperChars:   ['A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','T','W','X','Y','Z'],
+  ambUpper:     ['I','O','S','U','V'],
+  digitsChars:  ['2','3','4','6','7','8','9'],
+  ambDigits:    ['0','1','5'],
+}
